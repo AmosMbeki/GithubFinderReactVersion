1b579cde5e103d27701027452b80e20b8fdef389
AmosMbeki <amosmbeki@gmail.com>
2022-02-05 13:27:43 +0300

    Component, State and Props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React, {Component, Fragment} from "react";
+import React, {Component} from "react";
+import axios from "axios";
 import './App.css';
+import {Navbar} from './components/layout/Navbar'
+import {Users} from './components/users/Users'
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  }
+ async componentDidMount(){
+    
+   this.setState({loading: true});
+
+   const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+   this.setState({users: res.data, loading: false});
+
+  }
+
   render(){
     return (
-      <Fragment>
-       <h1>Hello from React</h1>
-       <h2>Good bye!</h2>
-      </Fragment>
+      <div className="App">
+       <Navbar />
+       <div className="container">
+        <Users loading={this.state.loading} users={this.state.users} />
+       </div>
+      </div>
     );
   }
  
